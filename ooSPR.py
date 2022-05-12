580ff7e05f6a9c6210f5900c429f65ddebb96927
Sam Flax <sam.flax2005@gmail.com>
2022-05-12 16:57:12 +1000

    changed player choice.

--- ooSPR.py
+++ ooSPR.py
@@ -1,21 +1,28 @@
 #scissors paper rock remake, object oriented.
 import random as r
 computerOptions = ('Win', 'Loss', 'Draw')
-againOptions = ('Y', 'N')
+playerOptions = ('s', 'p', 'r')
 
 class ScissorsPaperRock:
-
+#made on creation of the class.
     def __init__(self):
         self.name = input('enter your name: ')
         self.pScore = 0
         self.cScore = 0
-        self.again = True
         
     def makeChoice(self):
-        self.pChoice = input(f'{self.name}, please choose scissors, paper, or rock: ')
+#gives the player a 'choice' although it doesn't matter what the player chooses. Generates a random value for the computer.
+        incorrectChoice = True
+        while incorrectChoice is True:
+            self.pChoice = input(f'{self.name}, please choose S, P, or R: ').lower()
+            if self.pChoice not in playerOptions:
+                print(f'{self.name}, please enter a valid option.')
+            else:
+                incorrectChoice = False      
         self.cChoice = r.choice(computerOptions)
 
     def playGame(self):
+#determines the outcome based on computers choice.
         if self.cChoice == 'Win':
             print(self.name,'You Win!')
             self.pScore = self.pScore + 1
@@ -26,17 +33,21 @@ class ScissorsPaperRock:
             self.cScore = self.cScore + 1
         print(f'{self.name}, Your score is {self.pScore}. \nThe computers score is {self.cScore}')
 
-#Play the game
+#play the game
 SPR = ScissorsPaperRock()
 while True:
     SPR.makeChoice()
     SPR.playGame()
-    check = input('would you like to continue playing? Y or N').upper()
-    if check == 'Y':
-        pass
-    elif check == 'N':
-        print('Thanks for playing.')
-        quit()
-    else:
-        print('enter a valid response.')
-        continue
+    validInput = True
+#looping the game
+    while validInput is True:
+        check = input('Would you like to continue playing? Y or N').upper()
+        if check == 'Y':
+            validInput = False
+            continue
+        elif check == 'N':
+            print(f'Thanks for playing')
+            quit()
+        else:
+            print('Invalid response.')
+            continue
